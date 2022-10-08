29aee89e29e74324826c06da2c802e1add9ceb51
Yirga Mulaw <infoyirga12@gmail.com>
2022-10-08 07:59:16 +0300

    Stacks & Queues

---
+++ _push.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+* _push - function that adds node to the head stack
+* @head: head of the stack
+* @add: new value
+*
+* Return: Null
+*/
+void _push(stack_t **head, int new)
+{
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (temp)
+		temp->prev = new_node;
+	new_node->new = new;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
